2e5fe6087cb444c5ad12c09bdabfb7f439938903
ivi-dev <videviliyan@gmail.com>
2025-08-10 14:55:30 +0300

    Update volume mount paths

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,6 @@ CONTAINER_NAME=flags-cert-manager
 
 docker rm -f $CONTAINER_NAME
 docker run -d \
-       --mount type=volume,src=flags-cert-manager-private,dst=/etc/ssl/certs/self-signed/private \
-       --mount type=volume,src=flags-cert-manager-public,dst=/etc/ssl/certs/self-signed/public \
+       --mount type=volume,src=flags-cert-manager-private,dst=/etc/ssl/private/flags \
+       --mount type=volume,src=flags-cert-manager-public,dst=/etc/ssl/certs/flags \
        --name $CONTAINER_NAME $CONTAINER_NAME:latest
